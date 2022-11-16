007b0b976e80e6430c67d034a5eefbf762a7a7ef
Sridhar <rahul.ganesh.sridhar@accenture.com>
2022-11-16 16:22:50 +0530

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 import Layout from '../components/layout';
-import Navbar from '../components/NavBar';
 
 const IndexPage = () => {
   return (
@@ -8,6 +8,18 @@ const IndexPage = () => {
       <main>
         <p>Welcome to my Gatsby site!</p>
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <div className='images'>
+          <StaticImage
+            style={{ height: 300, width: 300 }}
+            src='../images/cat.jpg'
+            alt='Gatsby logo'
+          ></StaticImage>
+          <StaticImage
+            style={{ height: 300, width: 300 }}
+            alt='Clifford, a reddish-brown pitbull, dozing in a bean bag chair'
+            src='../images/clifford.jpg'
+          />
+        </div>
       </main>
     </Layout>
   );
