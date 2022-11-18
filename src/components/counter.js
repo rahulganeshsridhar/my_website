f1bcb5ea97fb5e2596e94e7981bf09b4faa80f8e
Sridhar <rahul.ganesh.sridhar@accenture.com>
2022-11-18 10:51:46 +0530

    fifth commit

---
+++ src/components/counter.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useContext } from 'react';
+import UserContext from './userContext';
+
+const center = {
+  textAlign: 'Center'
+};
+
+const btnStyle = {
+  paddingRight: '20px',
+  paddingLeft: '20px',
+  background: 'rgba(146, 43, 225, 1)',
+  backgroundRepeat: 'no-repeat'
+};
+
+const textStyle = {
+  fontSize: '50px',
+  padding: '20px',
+  verticalAlign: 'Middle',
+  color: 'black'
+};
+
+function Btn({ children, onClick }) {
+  return (
+    <button style={btnStyle} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+function Counter() {
+  const [counter, updateCounter] = useState(0);
+  const [randomText, updaterandomText] = useState('Counter');
+
+  function handleIncrement() {
+    updateCounter(counter + 1);
+  }
+
+  function handleDecrement() {
+    updateCounter(counter <= 0 ? 0 : counter - 1);
+  }
+
+  function handleReset() {
+    updateCounter(0);
+    updaterandomText(`Counter`);
+  }
+
+  useEffect(() => {
+    console.log('component lifecycle');
+    // updaterandomText(`Random text is updated`);
+  }, [counter, randomText]);
+  const user = useContext(UserContext);
+  return (
+    <div style={center}>
+      <Btn onClick={handleDecrement}>-</Btn>
+      <span style={textStyle}>{counter}</span>
+      <Btn onClick={handleIncrement}>+</Btn>
+      <div style={center}>
+        <Btn onClick={handleReset}>Reset</Btn>
+      </div>
+      <br />
+      <span style={{ padding: 10 }}>{randomText}</span>
+      <br />
+      <h1>Context API</h1>
+      <span>{user.name}</span>
+    </div>
+  );
+}
+
+export default Counter;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,35 @@
 import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
+import Counter from '../components/counter';
 import Layout from '../components/layout';
+import { UserProvider } from '../components/userContext';
 import { images } from '../styles/home.module.css';
 
 const IndexPage = () => {
+  const user = { name: 'Rahul Sridhar', loggedIn: true };
   return (
-    <Layout pageTitle='Home Page'>
-      <main>
-        <h2>Welcome to my Gatsby site!</h2>
-        <p>I'm making this by following the Gatsby Tutorial.</p>
-        <div className={images}>
-          <StaticImage
-            className='images'
-            src='../images/cat.jpg'
-            alt='Gatsby logo'
-          ></StaticImage>
-          <StaticImage
-            className='images'
-            alt='Clifford, a reddish-brown pitbull, dozing in a bean bag chair'
-            src='../images/clifford.jpg'
-          />
-        </div>
-      </main>
-    </Layout>
+    <UserProvider value={user}>
+      <Layout pageTitle='Home Page'>
+        <main>
+          <h2>Welcome to my Gatsby site!</h2>
+          <p>I'm making this by following the Gatsby Tutorial.</p>
+
+          <div className={images}>
+            <StaticImage
+              className='images'
+              src='../images/cat.jpg'
+              alt='Gatsby logo'
+            ></StaticImage>
+            <StaticImage
+              className='images'
+              alt='Clifford, a reddish-brown pitbull, dozing in a bean bag chair'
+              src='../images/clifford.jpg'
+            />
+            <Counter />
+          </div>
+        </main>
+      </Layout>
+    </UserProvider>
   );
 };
 
